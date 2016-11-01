83b86997c60968b85f57633d7b7869166ebb7e3a
hongyongming <hongyongming@rd.com>
2016-11-01 15:58:19 +0800

    处理google 的 jar冲突问题

---
+++ myjava/src/main/java/com/ming/testandlearn/concurrent/ratelimiter/MultiResourceRateLimiter.java
@@ -0,0 +1,87 @@
+package com.ming.testandlearn.concurrent.ratelimiter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by hongyongming on 2016/11/1.
+ */
+public class MultiResourceRateLimiter implements IMultiRateLimiter {
+    private static final ConcurrentMap<String, RateLimiter> resourceLimiterMap = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean require(String resourceName) {
+        RateLimiter limiter = resourceLimiterMap.get(resourceName);
+        if (limiter == null) {
+            // no limit ;
+            return true;
+        }
+        return limiter.tryAcquire();
+    }
+
+    @Override
+    public void release(String resourceName) {
+        //do nothing when use RateLimiter
+    }
+
+//    public static class RateLimitException extends Exception {
+//
+//        private static final long serialVersionUID = 1L;
+//
+//        private String resource;
+//
+//        public String getResource() {
+//            return resource;
+//        }
+//
+//        public RateLimitException(String resource) {
+//            super(resource + " should not be visited so frequently");
+//            this.resource = resource;
+//        }
+//
+//        @Override
+//        public synchronized Throwable fillInStackTrace() {
+//            return this;
+//        }
+//    }
+
+
+    public void updateResourceQps(String resource, double qps) {
+        RateLimiter limiter = resourceLimiterMap.get(resource);
+        if (limiter == null) {
+            limiter = RateLimiter.create(qps);
+            RateLimiter putByOtherThread = resourceLimiterMap.putIfAbsent(resource, limiter);
+            if (putByOtherThread != null) {
+                limiter = putByOtherThread;
+            }
+        }
+        limiter.setRate(qps);
+    }
+
+    public void removeResource(String resource) {
+        resourceLimiterMap.remove(resource);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        MultiResourceRateLimiter limiter = new MultiResourceRateLimiter();
+        limiter.updateResourceQps("url1", 10);
+        limiter.updateResourceQps("url2", 10);
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(limiter.require("url1")){
+                    System.out.println(String.format("【%s】访问成功",i+","+j));
+                }else
+                {
+                    System.out.println(String.format("【%s】访问量过高，你被限流了",i+","+j));
+                }
+            }
+            Thread.sleep(100);
+        }
+    }
+
+}
